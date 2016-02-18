70d0673dbb8fbaaf73d795015b0de19ea8e97d8c
na3ksg <superkasago@yahoo.co.jp>
2016-02-18 18:48:23 +0900

    エスケープシーケンスの練習

--- src/javalesson/JavaLesson.java
+++ src/javalesson/JavaLesson.java
@@ -1,7 +1,9 @@
 package javalesson;
 public class JavaLesson {
-    public static void main(String[] args) {
-        // TODO code application logic here
-     }
-    
+	public static void main(String[] args) {
+		// TODO code application logic here
+		System.out.println("私は\"Java\"を勉強しています。");
+		System.out.println("私は\"Java\"を勉強しています。");
+		System.out.println("私は\"Java\"を勉強しています。");
+	}
 }
