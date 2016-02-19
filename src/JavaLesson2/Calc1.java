ac2c2b9d51b3bc5ab78880c62bfd918106d335bd
na3ksg <superkasago@yahoo.co.jp>
2016-02-19 16:37:34 +0900

    ライセンスヘッダの削除

---
+++ src/JavaLesson2/Calc1.java
@@ -0,0 +1,22 @@
+package JavaLesson2;
+public class Calc1 {
+	public static void main(String[] args) {
+		// TODO code application logic here
+//		System.out.println("0×0=" + (0 * 0));
+//		System.out.println("1×1=" + (1 * 1));
+//		System.out.println("2×2=" + (2 * 2));
+//		System.out.println("3×3=" + (3 * 3));
+//		System.out.println("4×4=" + (4 * 4));
+//		System.out.println("5×5=" + (5 * 5));
+//		System.out.println("6×6=" + (6 * 6));
+//		System.out.println("7×7=" + (7 * 7));
+//		System.out.println("8×8=" + (8 * 8));
+//		System.out.println("9×9=" + (9 * 9));
+//		System.out.println("10×=10" + (10 * 10));
+
+		for (int i = 0; i <= 10; i++) {
+			System.out.println(i + "×" + i + "=" + (i * i));
+		}
+	}
+	
+}
---
+++ src/javalesson1/JavaLesson.java
@@ -0,0 +1,9 @@
+package javalesson1;
+public class JavaLesson {
+	public static void main(String[] args) {
+		// TODO code application logic here
+		System.out.println("私は\"java\"を勉強しています。");
+		System.out.println("私は\"java\"を勉強しています。");
+		System.out.println("私は\"java\"を勉強しています。");
+	}
+}
