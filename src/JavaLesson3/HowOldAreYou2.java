8e178e4aa68a3de486b43c9dcd1a4f12e5190f21
na3ksg <superkasago@yahoo.co.jp>
2016-02-19 18:36:28 +0900

    3-2

---
+++ src/JavaLesson3/HowOldAreYou2.java
@@ -0,0 +1,32 @@
+package JavaLesson3;
+import java.io.*;
+public class HowOldAreYou2 {
+	public static void main(String[] args) {
+		// TODO code application logic here
+		BufferedReader reader = new BufferedReader (new InputStreamReader(System.in));
+		try {
+			String line;
+			// 1人目
+			System.out.println("1人目の名前を教えてね");
+			String name1 = reader.readLine();
+			System.out.println(name1 + "さんの年齢を教えてね");
+			line = reader.readLine();
+			int age1 = Integer.parseInt(line);
+			// 2人目
+			System.out.println("2人目の名前を教えてね");
+			String name2 = reader.readLine();
+			System.out.println(name2 + "さんの年齢を教えてね");
+			line = reader.readLine();
+			int age2 = Integer.parseInt(line);
+			// 平均
+			double average = (age1 + age2) / 2.0;
+			System.out.println(name1 + "さんと" + name2 + "さんの年齢の平均は" + average + "です。");
+		} catch (IOException e) {
+			System.out.println(e);
+		} catch (NumberFormatException e) {
+			System.out.println("年齢が正しくないよ！");
+		}
+		
+	}
+	
+}
