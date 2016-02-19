e2560700e5e856f75ea9d88a01ac01351b5f3ce6
na3ksg <superkasago@yahoo.co.jp>
2016-02-19 18:21:24 +0900

    3-1

---
+++ src/JavaLesson3/Light.java
@@ -0,0 +1,7 @@
+package JavaLesson3;
+public class Light {
+	public static void main(String[] args) {
+		// TODO code application logic here
+		System.out.println("光が１日に進む距離は" + (300000L * 60 * 60 * 24) + "kmだよ。");
+	}
+}
