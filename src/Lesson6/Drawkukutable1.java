c1fd6f4e52712f367b1bcc64965d0a85f6661771
na3ksg <superkasago@yahoo.co.jp>
2016-02-21 18:31:52 +0900

    6-4

---
+++ src/Lesson6/Drawkukutable1.java
@@ -0,0 +1,13 @@
+package Lesson6;
+public class Drawkukutable1 {
+	public static void main(String[] args) {
+		// TODO code application logic here
+		for (int i = 1; i < 10; i++) {
+			for (int j = 1; j < 10; j++) {
+				System.out.println(i + "*" + j + "=" + (i * j));
+			}
+			System.out.println("");
+		}
+	}
+	
+}
