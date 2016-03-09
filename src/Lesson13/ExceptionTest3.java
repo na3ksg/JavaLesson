60f168c5dcef5acf769ffb7042ff82ac5a26af42
na3ksg <superkasago@yahoo.co.jp>
2016-03-09 13:38:33 +0900

    スタックトレースを使う

---
+++ src/Lesson13/ExceptionTest3.java
@@ -0,0 +1,23 @@
+package Lesson13;
+public class ExceptionTest3 {
+	public static void main(String[] args) {
+		// TODO code application logic here
+		int[] myarray = new int[3];
+		try {
+			System.out.println("代入するよ");
+			myAssign(myarray, 100, 0);
+			System.out.println("代入したよ");
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("代入できなかったよ");
+			System.out.println("例外は" + e + "だよ");
+			e.printStackTrace();
+		}
+		System.out.println("終了するよ");
+	}
+	static void myAssign(int[] arr, int index, int value) {
+		System.out.println("myAssignに来たよ");
+		arr[index] = value;
+		System.out.println("myAssignから帰るよ");
+	}
+	
+}
