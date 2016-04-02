36798ecbbe458957ce7c1df512ded6aacbb82af3
na3ksg <superkasago@yahoo.co.jp>
2016-04-02 23:33:38 +0900

    オブジェクト指向の練習3章
    完成

--- src/Shop/ShopKeeper.java
+++ src/Shop/ShopKeeper.java
@@ -2,11 +2,24 @@ package Shop;
 public class ShopKeeper {
 	String name_;
 
-	public void checkPrice() {
+	public void checkPrice(CarToy kuruma1, CarToy kuruma2, TrainToy train1) {
+		System.out.println("私が店長の" + name_ + "だ！これから価格をチェックするぞ！");
+		System.out.println("商品コード：" + kuruma1.code_ + "の価格は" + kuruma1.price_ + "です。");
+		System.out.println("商品コード：" + kuruma2.code_ + "の価格は" + kuruma2.price_ + "です。");
+		System.out.println("商品コード：" + train1.code_ + "の価格は" + train1.price_ + "です。");
+		System.out.println("\n");
 		
 	}
 
-	public void doDemo() {
+	public void doDemo(CarToy kuruma1, CarToy kuruma2, TrainToy train1) {
+		System.out.println("私が店長の" + name_ + "だ！これからデモをするぞ！");
+		System.out.println("クルマ1を走らせるぞ！");
+		kuruma1.go();
+		System.out.println("クルマ2を走らせるぞ！");
+		kuruma2.go();
+		System.out.println("電車を走らせるぞ！");
+		train1.go();
+		System.out.println("\n");
 		
 	}
 	
--- src/Shop/ToyShop.java
+++ src/Shop/ToyShop.java
@@ -15,20 +15,10 @@ public class ToyShop {
 		train1.code_ = "t001";
 		train1.price_ = 700;
 
-		System.out.println(suzuki.name_);
-		System.out.println(car1.code_);
-		System.out.println(car1.price_);
-		System.out.println(car2.code_);
-		System.out.println(car2.price_);
-		System.out.println(train1.code_);
-		System.out.println(train1.price_);
+		suzuki.checkPrice(car1, car2, train1);
 
-		car1.go();
-		car1.light();
-		car2.go();
-		car2.light();
-		train1.go();
-		train1.horn();
+		suzuki.doDemo(car1, car2, train1);
+
 	}
 	
 }
